95d07210794e43893ccb8bfc411b6b71f705156b
Nora <nora.munkhsukh@gmail.com>
2026-08-28 17:18:55 +0800

    feat:init

---
+++ src/main/java/mn/icode/SpringBootLearningCapstoneApplication.java
@@ -0,0 +1,13 @@
+package mn.icode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLearningCapstoneApplication.class, args);
+	}
+
+}
