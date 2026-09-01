3e5849b5edddc07f2e3cc3290152f4ad78e04710
Nora <nora.munkhsukh@gmail.com>
2026-09-01 17:37:50 +0800

    feat: started on category service

--- src/main/java/mn/icode/service/CategoryService.java
+++ src/main/java/mn/icode/service/CategoryService.java
@@ -1,7 +1,11 @@
 package mn.icode.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import mn.icode.model.Category;
 import mn.icode.repository.CategoryRepository;
 
 @Service
@@ -12,6 +16,24 @@ public class CategoryService {
     public CategoryService(CategoryRepository categoryRepository) {
         this.categoryRepository = categoryRepository;
     }
+
+    public List<Category> findAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findCategoryById(Long id) {
+        return  categoryRepository.findById(id).orElseThrow();
+    }
+    
+    @Transactional
+    public Category create(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    
+    
+
+    
 
     
 }
